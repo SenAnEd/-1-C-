2c837978a2f16ffe57b39097cabc2aac442806fa
SenAnEd <senaned@yandex.ru>
2022-04-18 23:44:55 +0300

    Удалила лишний файл

---
+++ Homework 2/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая выводит случайное трехзначное число и удаляет вторую цифру этого числа
+Console.Clear ();
+
+int a = new Random().Next(100,1000);
+
+System.Console.WriteLine(a);
+
+int b = a / 100;
+
+int c = a % 10;
+
+System.Console.WriteLine($"{b}{c}");
+
+ 
