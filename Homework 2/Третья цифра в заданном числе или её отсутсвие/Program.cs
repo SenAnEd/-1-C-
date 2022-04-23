966100998dc4eea57842b255ce186e21ee28fb33
SenAnEd <senaned@yandex.ru>
2022-04-23 23:39:26 +0300

    Решила задачу про выходные

--- Homework 2/Третья цифра в заданном числе или её отсутсвие/Program.cs
+++ Homework 2/Третья цифра в заданном числе или её отсутсвие/Program.cs
@@ -5,17 +5,17 @@ Console.Clear();
 Console.WriteLine("Введите число: ");
 
 int a = Convert.ToInt32(Console.ReadLine());
-if (a/100==0)
+if (a / 100 == 0)
 {
-    System.Console.WriteLine($"Третьей цифры в заданном числе нет");
+    System.Console.WriteLine("Третьей цифры в заданном числе нет");
 }
 else
 {
- while(a/1000> 0)
- {
-    a= a/10;
- }
-System.Console.WriteLine($"Третья цифра в заданном числе: {a % 10}");
+    while (a / 1000 > 0)
+    {
+        a = a / 10;
+    }
+    System.Console.WriteLine($"Третья цифра в заданном числе: {a % 10}");
 }
 
 
