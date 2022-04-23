110f7c8f7d83dac20a87f93e46f53af7b359fdad
SenAnEd <senaned@yandex.ru>
2022-04-24 01:29:11 +0300

    Решила задачу расстояние между двух точек в 3D пространстве

---
+++ Homework 3/Расстояние между точками в 3D пространстве/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек 
+//и находит расстояние между ними в 3D пространстве.
+
+int x1 = 4;
+int y1 = 6;
+int c1 = 9;
+int x2 = 5;
+int y2 = 9;
+int c2 = 8;
+
+double length = Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2) + Math.Pow(c1 - c2, 2));
+
+System.Console.WriteLine(length);
