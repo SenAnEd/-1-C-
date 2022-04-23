18db272c4600e54c5f86f43cec1fd6943a913568
SenAnEd <senaned@yandex.ru>
2022-04-24 02:23:07 +0300

    Решила задачу про палиндром

---
+++ Homework 3/Поиск полиндрома/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число 
+//и проверяет, является ли оно палиндромом.
+Console.Clear();
+
+Console.Write("Введите число: ");
+
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a / 10000 == a % 10)
+{
+    if (a / 1000 % 10 == a / 10 % 10)
+    {
+        System.Console.WriteLine("Число является палиндромом");
+    }
+    else
+    {
+        System.Console.WriteLine("Число не является палиндромом");
+    }
+}
+else
+{
+    System.Console.WriteLine("Число не является палиндромом");
+}
