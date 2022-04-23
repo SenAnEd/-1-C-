d81e37255af7da098c0294230d79061fee172605
SenAnEd <senaned@yandex.ru>
2022-04-23 20:34:36 +0300

    Дорешала задачу про сумму

--- Произведение чисел от 1 до N/Program.cs
+++ Произведение чисел от 1 до N/Program.cs
@@ -1,7 +1,7 @@
 ﻿// Напишите программу которая на вход принимает число N и считает произведение чисел от 1 до N
 Console.Clear();
 
-System.Console.WriteLine("Введите число N: ");
+System.Console.Write("Введите число N: ");
 
 int N = int.Parse(System.Console.ReadLine());
 int comp = 1;
@@ -10,7 +10,7 @@ int i = 1;
 while(i <= N)
 {
     comp = i*comp;
-    Console.WriteLine($"{comp}");
+    Console.WriteLine($"{comp} ");
     i++;
 }
-System.Console.WriteLine($"Произведение от 1 до N равно {comp}");
+System.Console.WriteLine($"Произведение от 1 до N равно: {comp}");
