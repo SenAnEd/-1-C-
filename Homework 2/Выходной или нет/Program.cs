b052b08ef57b8f2b7a5743f1baccc5497549a7d0
SenAnEd <senaned@yandex.ru>
2022-04-23 23:40:43 +0300

    Решила задачу про выходные

---
+++ Homework 2/Выходной или нет/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным
+
+Console.Clear();
+
+int a = new Random().Next(1, 8);
+
+System.Console.WriteLine(a);
+
+if (a == 6 | a == 7)
+{
+    System.Console.WriteLine("Выходной день");
+}
+else
+    System.Console.WriteLine("Не выходной день");
+
