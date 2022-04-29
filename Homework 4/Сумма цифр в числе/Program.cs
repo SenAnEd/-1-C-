36a7d0ad671f9b881994018e92aad03cfd9d5674
SenAnEd <senaned@yandex.ru>
2022-04-29 21:40:27 +0300

    Решение задачи сумма цифр в числе

---
+++ Homework 4/Сумма цифр в числе/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+Console.Clear();
+System.Console.Write("Введите число А: ");
+int A = int.Parse(Console.ReadLine());
+int B = A % 10;
+int C = 0;
+int sum = 0;
+
+while (A/10 > 0)
+{
+    A = A/10;
+    C = A % 10;
+    sum = sum+C;
+}
+System.Console.WriteLine($"{sum+B} ");
+
