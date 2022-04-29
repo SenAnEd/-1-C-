dd56ac56d4088aa39af45eb51f2d296dec33b5d1
SenAnEd <senaned@yandex.ru>
2022-04-29 20:01:15 +0300

    Решение задачи количество цифр в числе

---
+++ Количество цифр в числе/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход число и выдает количество цифр в числе
+
+// Первый способ
+
+Console.Clear();
+System.Console.Write("Введите число А: ");
+int A = int.Parse(Console.ReadLine());
+int count = 0;
+while(A>0)
+{
+    A=A/10;
+    count ++;
+}
+System.Console.WriteLine($"Количество цифр в заданном числе {count}: ");
+
