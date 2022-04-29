afa641210c7218666ec179bb2d677f792e19c58c
SenAnEd <senaned@yandex.ru>
2022-04-29 20:07:21 +0300

    Добавила второй способ решения задачи количество цифр в числе через функцию

--- Количество цифр в числе/Program.cs
+++ Количество цифр в числе/Program.cs
@@ -2,14 +2,20 @@
 
 // Первый способ
 
+// Console.Clear();
+// System.Console.Write("Введите число А: ");
+// int A = int.Parse(Console.ReadLine());
+// int count = 0;
+// while(A>0)
+// {
+//     A=A/10;
+//     count ++;
+// }
+// System.Console.WriteLine($"Количество цифр в заданном числе {count}: ");
+
+// Второй способ через функцию:
+
 Console.Clear();
 System.Console.Write("Введите число А: ");
-int A = int.Parse(Console.ReadLine());
-int count = 0;
-while(A>0)
-{
-    A=A/10;
-    count ++;
-}
-System.Console.WriteLine($"Количество цифр в заданном числе {count}: ");
-
+int A=Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine(A.ToString().Length);
