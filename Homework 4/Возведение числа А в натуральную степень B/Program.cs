d15b1e1b5dfeed0433ede5239bc8431440aa368d
SenAnEd <senaned@yandex.ru>
2022-04-29 22:11:15 +0300

    Решение задачи возведение числа в натуральную степень

---
+++ Homework 4/Возведение числа А в натуральную степень B/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите цикл, который принимает на вход два числа A и B, и возводит число A в натуральную степень B
+
+Console.Clear();
+System.Console.Write("Введите число А: ");
+int A = int.Parse(Console.ReadLine());
+System.Console.Write("Введите число B: ");
+int B = int.Parse(Console.ReadLine());
+int N = A;
+for (int i = 1; i < B; i++)
+{
+    N = A*N;
+}
+Console.WriteLine($"Число {A} в степени {B} равно {N}");
