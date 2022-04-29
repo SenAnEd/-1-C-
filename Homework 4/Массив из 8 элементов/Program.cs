06748579a47adfa404e41de221ede01ef102e7d3
SenAnEd <senaned@yandex.ru>
2022-04-29 20:18:02 +0300

    Решение задачи массив из 8 элементов

---
+++ Homework 4/Массив из 8 элементов/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+Console.Clear();
+int[] mass = new int[8];
+for (int i = 0; i < mass.Length; i++)
+{
+    mass[i] = new Random().Next(int.MinValue, int.MaxValue);
+    System.Console.Write(mass[i] + " ");
+
+}
