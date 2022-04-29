03c02e5b5d2b73a7df2aa204678d2c1a9a3faa4a
SenAnEd <senaned@yandex.ru>
2022-04-29 19:36:09 +0300

    Добавила второй способ решения задачи произведение чисел от 1 до N через цикл for

--- Произведение чисел от 1 до N/Program.cs
+++ Произведение чисел от 1 до N/Program.cs
@@ -1,14 +1,29 @@
 ﻿// Напишите программу которая на вход принимает число N и считает произведение чисел от 1 до N
-Console.Clear();
-System.Console.Write("Введите число N: ");
-int N = int.Parse(System.Console.ReadLine());
-int comp = 1;
-int i = 1;
 
-while(i <= N)
+// Первый способ через цикл while:
+// Console.Clear();
+// System.Console.Write("Введите число N: ");
+// int N = int.Parse(System.Console.ReadLine());
+// int comp = 1;
+// int i = 1;
+
+// while(i <= N)
+// {
+//     comp = i*comp;
+//     Console.WriteLine($"{comp} ");
+//     i++;
+// }
+// System.Console.WriteLine($"Произведение от 1 до N равно: {comp}");
+
+// Второй способ через цикл for:
+
+Console.Clear();
+Console.Write("Введите число N: ");
+int N = int.Parse(Console.ReadLine());
+int pN = 1;
+for(int i = 1; i<=N; i++)
 {
-    comp = i*comp;
-    Console.WriteLine($"{comp} ");
-    i++;
+Console.WriteLine(i);
+pN = pN*i;
 }
-System.Console.WriteLine($"Произведение от 1 до N равно: {comp}");
+Console.WriteLine(pN);
