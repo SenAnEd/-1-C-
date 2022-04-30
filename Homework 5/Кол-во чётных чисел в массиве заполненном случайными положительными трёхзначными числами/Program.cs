48b7ec0545776efab021689e152ca957445a5866
SenAnEd <senaned@yandex.ru>
2022-04-30 23:32:28 +0300

    Решение задачи Кол-во четных чисел в массиве заполненном случайными положительными трехзначными числами

--- Homework 5/Кол-во чётных чисел в массиве заполненном случайными положительными трёхзначными числами/Program.cs
+++ Homework 5/Кол-во чётных чисел в массиве заполненном случайными положительными трёхзначными числами/Program.cs
@@ -1,4 +1,14 @@
-﻿void PrintArray (int []array)
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, 
+// которая покажет количество чётных чисел в массиве.
+
+void FillArray(int [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array [i] = new Random().Next(100,1000);
+    }
+}
+void PrintArray (int []array)
 {
     for(int i=0; i < array.Length; i++)
     {
@@ -6,3 +16,14 @@
     }
     System.Console.WriteLine();
 }
+
+Console.Clear();
+int [] mass = new int [5];
+FillArray(mass);
+PrintArray(mass);
+int count=0;
+for (int i = 0; i < mass.Length; i++)
+{
+  if(mass[i]%2==0) count++;
+}
+System.Console.WriteLine($"Количество четных чисел в массиве {count}");
