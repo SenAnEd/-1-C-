65eb46f6478833b8fd84d537a1cb8a38123882b3
SenAnEd <senaned@yandex.ru>
2022-04-30 23:56:24 +0300

    Решение задачи Сумма элементов на нечетных позициях в одномерном массиве заполненном случайными числами

---
+++ Homework 5/Сумма элементов на нечетных позициях в одномерном массиве заполненном случайными числами/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, 
+// стоящих на нечётных позициях.
+
+void FillArray(int [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array [i] = new Random().Next(1,10);
+    }
+}
+void PrintArray (int []array)
+{
+    for(int i=0; i < array.Length; i++)
+    {
+        Console.Write(array [i] + " ");
+    }
+    System.Console.WriteLine();
+}
+
+Console.Clear();
+int [] mass = new int [5];
+FillArray(mass);
+PrintArray(mass);
+int summ=0;
+for (int i = 1; i < mass.Length; i=i+2)
+{
+  summ += mass [i];
+}
+System.Console.WriteLine($"Сумма элементов стоящих на нечетных позициях = {summ} ");
